1f390c52ce291f82130dc80a69e894ebba65ef92
fran-man <fran.man@hotmail.co.uk>
2020-02-04 20:48:50 +0000

    Add initial transformer file

---
+++ MsgTransformer.py
@@ -0,0 +1,9 @@
+from kafka import KafkaProducer,KafkaConsumer
+
+prod = KafkaProducer(bootstrap_servers='kafka:9092',max_block_ms=5000)
+
+consumer = KafkaConsumer(bootstrap_servers='kafka:9092')
+consumer.subscribe(['TopicIn'])
+
+for msg in consumer:
+    print(msg)
